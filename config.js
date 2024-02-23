b8ec7297630bdd66225be8dd058b5abadeb09dd0
ViCongMinh <150504257+ViCongMinh@users.noreply.github.com>
2024-02-23 22:19:15 +0700

    Add files via upload
    tesst

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Huyền:333';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100022641384205';
+const eventName = 'Chúc Mừng Sinh Nhật ';
+const titleCard = 'Bùi Mai Huyền';
+const contentCard = 'Tui mong bạn iu của tui luôn tươi trẻ, xinh đẹp, may mắn và gặt hái được nhiều thành công trong tương lai nha:33. Mẫn Nhi trong lòng tuii:333';
+const giftImage = 'hot-girl1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
